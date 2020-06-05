f8dde48c6b34e38cd88c95b8a368c3adc2914283
Kaliades <Szym.Kulinski@gmail.com>
2020-06-05 16:25:58 +0200

    Solved issue with lab33.

--- src/cw_33/lab_util.py
+++ src/cw_33/lab_util.py
@@ -14,9 +14,9 @@ def get_data_frame_from_csv(file_name):
 
 def plot_all(df):
     sns.set(style="darkgrid")
-    sns.lmplot(y="U[V] R=2cm", x="I[A] R=2cm", data=df, order=2,
-               ci=None, scatter_kws={"s": 50}, height=10)
     sns.lmplot(y="U[V] R=3cm", x="I[A] R=3cm", data=df, order=2,
+               ci=None, scatter_kws={"s": 50}, height=10)
+    sns.lmplot(y="U[V] R=4cm", x="I[A] R=4cm", data=df, order=2,
                ci=None, scatter_kws={"s": 50}, height=10)
     sns.lmplot(y="U[V] R=5cm", x="I[A] R=5cm", data=df, order=2,
                ci=None, scatter_kws={"s": 50}, height=10)
@@ -28,14 +28,12 @@ def fun(x, a):
 
 def regression(data_frame):
     models = dict()
-    r = 2
+    r = 3
     for i in range(0, data_frame.columns.size - 1, 2):
         x = data_frame[data_frame.columns[i]].dropna().to_numpy()  # I[A]
         y = data_frame[data_frame.columns[i + 1]].dropna().to_numpy()
         popt, pcov = curve_fit(fun, x, y)  #
         model = np.poly1d([popt[0], 0, 0])
-        if r == 4:
-            r = 5
         models[r] = model
         r += 1
     return models
